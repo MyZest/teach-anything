0e1bca6d554bf245b0b5ef1dcfad7e1ac46b93e2
zest <mrwangl2018@163.com>
2023-02-24 22:41:50 +0800

    删除版权

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,15 +22,7 @@ class MyDocument extends Document {
             name="twitter:description"
             content="Teach you anything in seconds use AI."
           />
-          <meta
-            property="og:image"
-            content="https://www.teach-anything.com/og-image.png"
-          />
-          <meta name="twitter:card" content="summary_large_image"/>
-          <meta name="twitter:site" content="@lvwzhen"/>
-          <meta name="twitter:title" content="Teach Anything"/>
-          <meta name="twitter:description" content="Teach you anything in seconds use AI"/>
-          <meta name="twitter:image" content="https://www.teach-anything.com/og-image.png"/>
+        
         </Head>
         <body>
           <Main />
